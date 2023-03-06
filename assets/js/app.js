df0d4065b39a49b93396440baae0a8381eecc490
Doanhaiduy <haiduytbt2k3@gmail.com>
2023-03-06 11:59:59 +0700

    create feature btn-page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,22 @@
-const API_URL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+let i = JSON.parse(localStorage.getItem("counter")) || 1;
+
 const list = document.querySelector(".movies");
-fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-        data.results.forEach((element) => {
-            let movie = document.createElement("div");
-            movie.className = "movie-item";
-            movie.innerHTML = `<img src="${IMG_PATH}${element.poster_path}" alt="" />
+const next = document.querySelector(".btn.next");
+const pre = document.querySelector(".btn.pre");
+const pages = document.querySelectorAll(".btn.btn-page");
+function render() {
+    let API_URL =
+        "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=" +
+        i;
+    const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+    const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+            data.results.forEach((element) => {
+                let movie = document.createElement("div");
+                movie.className = "movie-item";
+                movie.innerHTML = `<img src="${IMG_PATH}${element.poster_path}" alt="" />
                     <div class="info info-1">
                         <h2 class="heading">${element.title}</h2>
                         <span>${element.vote_average}</span>
@@ -20,32 +27,87 @@ fetch(API_URL)
                             ${element.overview}
                         </p>
                     </div>`;
-            list.appendChild(movie);
-        });
-    })
-    .then(() => {
-        const search = document.querySelector(".search");
-        const list_movies = document.querySelectorAll(".movie-item");
-        search.addEventListener("input", () => {
-            let ok = false;
-            let error = document.querySelector(".no-find");
-            const name = search.value.toLowerCase();
-            list_movies.forEach((item) => {
-                if (item.querySelector("h2").innerText.toLowerCase().includes(name)) {
-                    item.style.display = "block";
-                    ok = true;
+                list.appendChild(movie);
+                if (i <= 1) {
+                    pre.classList.add("active");
+                    // return;
                 } else {
-                    item.style.display = "none";
+                    pre.classList.remove("active");
+                }
+                if (i >= 10) {
+                    next.classList.add("active");
+                } else {
+                    next.classList.remove("active");
                 }
             });
-            if (ok) {
-                error.classList.remove("active");
-            } else {
-                error.classList.add("active");
-                error.querySelector("strong").innerText = name;
-            }
+        })
+        .then(() => {
+            const search = document.querySelector(".search");
+            const list_movies = document.querySelectorAll(".movie-item");
+            search.addEventListener("input", () => {
+                let ok = false;
+                let error = document.querySelector(".no-find");
+                const name = search.value.toLowerCase();
+                list_movies.forEach((item) => {
+                    if (item.querySelector("h2").innerText.toLowerCase().includes(name)) {
+                        item.style.display = "block";
+                        ok = true;
+                    } else {
+                        item.style.display = "none";
+                    }
+                });
+                if (ok) {
+                    error.classList.remove("active");
+                } else {
+                    error.classList.add("active");
+                    error.querySelector("strong").innerText = name;
+                }
+            });
+        })
+        .catch((error) => {
+            console.log("Error");
         });
-    })
-    .catch((error) => {
-        console.log("Error");
+}
+
+render();
+document.querySelector(".btn.btn-page.active").classList.remove("active");
+pages[i - 1].classList.add("active");
+next.addEventListener("click", () => {
+    if (i >= 10) {
+        next.classList.add("active");
+    } else {
+        i++;
+        list.innerHTML = "";
+        next.classList.remove("active");
+        render();
+        pages[i - 1].classList.add("active");
+        document.querySelector(".btn.btn-page.active").classList.remove("active");
+        localStorage.setItem("counter", i);
+    }
+});
+
+pre.addEventListener("click", (event) => {
+    if (i == 1) {
+        pre.classList.add("active");
+    } else {
+        i--;
+        list.innerHTML = "";
+        render();
+        document.querySelector(".btn.btn-page.active").classList.remove("active");
+        pages[i - 1].classList.add("active");
+        localStorage.setItem("counter", i);
+    }
+});
+
+pages.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (!item.classList.contains("active")) {
+            document.querySelector(".btn.btn-page.active").classList.remove("active");
+            item.classList.add("active");
+            localStorage.setItem("counter", parseInt(item.getAttribute("data-set")));
+            window.location = "index.html";
+        } else {
+            return;
+        }
     });
+});
